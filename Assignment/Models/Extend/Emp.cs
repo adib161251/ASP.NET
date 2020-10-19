684d68ac318aa7cc3b26f4bb89c2c3387a1047ee
Adib Arman <58285803+adib161251@users.noreply.github.com>
2020-10-20 01:05:58 +0600

    Add files via upload

---
+++ Assignment/Models/Extend/Emp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment.Models
+{
+    [MetadataType(typeof(EmployeeMetadata))]
+    public partial class Emp
+    {
+    }
+    public class EmployeeMetadata
+    {
+        [Required (AllowEmptyStrings =false, ErrorMessage ="Input is Required")]
+        public string Gender { get; set; }
+        [Required(AllowEmptyStrings =false , ErrorMessage ="Input is Required")]
+        public string Department { get; set; }
+    }
+}
