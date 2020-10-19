dfe4995a7333452ed9a16b1ea98720f64e74c179
Adib Arman <58285803+adib161251@users.noreply.github.com>
2020-10-20 00:56:15 +0600

    Add files via upload

---
+++ Assignment/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment.Models
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+
+    }
+}
