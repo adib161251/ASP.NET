59ad22fc19ac4a53206d503d1c8145707bb6595c
Adib Arman <58285803+adib161251@users.noreply.github.com>
2020-10-20 00:50:24 +0600

    Add files via upload

---
+++ Assignment/Controllers/MoviesController.cs
@@ -0,0 +1,51 @@
+﻿using Assignment.Models;
+using Assignment.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            movie mv = new movie()
+            {
+                Name="Injustice"
+            };
+
+            var customers = new List<Customer>()
+            {
+                new Customer{name="Adib Arman"},
+                new Customer{name="Silicon Valle"},
+                new Customer{name="Fabliha Rahman"}
+
+            };
+
+            var viewmodel = new RandomMovieViewModel()
+            {
+                Movie = mv,
+                Customers = customers
+            };
+            return View(viewmodel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id" + id);
+        }
+
+        public ActionResult Index(int? pageIndex,string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrEmpty(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
+        }
+    }
+}
---
+++ Assignment/Controllers/mployeeController.cs
@@ -0,0 +1,108 @@
+﻿using Assignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment.Controllers
+{
+    public class mployeeController : Controller
+    {
+        // GET: mployee
+        public ActionResult CRUD()
+        {
+            return View();
+        }
+        public ActionResult GetInfo()
+        {
+            using(Database1Entities3 db = new Database1Entities3())
+            {
+                var employees = db.Emps.OrderBy(a=>a.Gender).ToList();
+                return Json(new { data = employees }, JsonRequestBehavior.AllowGet);
+            }
+            
+        }
+
+        [HttpGet]
+        public ActionResult Save(int id)
+        {
+            using (Database1Entities3 dc = new Database1Entities3())
+            {
+                var v = dc.Emps.Where(a => a.Id == id).FirstOrDefault();
+                return View(v);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Save(Emp employee)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (Database1Entities3 dc = new Database1Entities3())
+                {
+                    if (employee.Id > 0)
+                    {
+                        //Edit 
+                        var v = dc.Emps.Where(a => a.Id == employee.Id).FirstOrDefault();
+                        if (v != null)
+                        {
+                            v.Id = employee.Id;
+                            v.Gender = employee.Gender;
+                            v.Department = employee.Department;
+                            v.City = employee.City;
+                         
+                        }
+                    }
+                    else
+                    {
+                        //Save
+                        dc.Emps.Add(employee);
+                    }
+                    dc.SaveChanges();
+                    status = true;
+                    return View();
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+        //for confirmation
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            using(Database1Entities3 db = new Database1Entities3())
+            {
+                var v = db.Emps.Where(a => a.Id == id).FirstOrDefault();
+                if(v != null)
+                {
+                   return View(v);
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+        }
+        //for delete
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteEmployee(int id)
+        {
+            bool status = false;
+            using (Database1Entities3 db = new Database1Entities3())
+            {
+                var v = db.Emps.Where(a => a.Id == id).FirstOrDefault();
+                if (v != null)
+                {
+                    db.Emps.Remove(v);
+                    db.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+
+    }
+}
